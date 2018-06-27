76e38435f5851e8364afbf176fabea119c4cac5f
Jacqui <jacqui.morrison.13@gmail.com>
2018-06-27 22:18:42 +0000

    updated CSS and overall HTML

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.recipe_list, name='recipe_list'),
+    re_path(r'^recipe/(?P<pk>\d+)/$', views.recipe_detail, name='recipe_detail'),
     ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 from django.utils import timezone
 
 def recipe_list(request):
     recipes = Recipe.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'recipe/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipe/recipe_detail.html', {'recipe': recipe})
