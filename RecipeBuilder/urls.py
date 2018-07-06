c44315ab0dca3970e0a664a3366105bcd1a7a5cd
Jacqui <jacqui.morrison.13@gmail.com>
2018-07-06 21:44:18 +0000

    login page bug fix

--- RecipeBuilder/urls.py
+++ RecipeBuilder/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/$', views.login, name='login'),
+    path('accounts/login/', views.login, name='login'),
     path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
     path('', include('recipe.urls')),
     ]
